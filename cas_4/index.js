95893c545d9cc2de2c1a3d107b58999d608c5192
hristijankostadinoski <hristijan@2coders.com>
2023-11-13 21:46:23 +0100

    ternary operator, spread operator and destructuring

--- cas_4/index.js
+++ cas_4/index.js
@@ -36,11 +36,70 @@ let firstNumberBiggerThenSixty = evenNumbers.find((num) => num > 60)
 console.log(firstNumberBiggerThenSixty)
 
 //some()
-let numArray = [11, 1, 3, 17, 10, 11, 87, 11]
+let numArray = [11, 1, 3, 17, 10, 30, 87, 11]
 let numArrayHasEvenNumber = numArray.some((num) => num % 2 === 0)
 let numArrayHasOddNumber = numArray.some((num) => num % 2 !== 0)
 
 console.log(numArrayHasEvenNumber)
 console.log(numArrayHasOddNumber)
 
+const isEvenNumber = (num) => {
+  return num % 2 === 0
+}
+
+let randomEvenNumber = numArray.filter(isEvenNumber).every(isEvenNumber)
+console.log(randomEvenNumber)
+
 // Spread Operator, Ternary Operator and Destructuring
+
+// spread operator(...)
+let originalArray = [10, 20, 30, 40, 50]
+let copiedArray = [...originalArray]
+
+console.log(originalArray, copiedArray)
+
+let array1 = [1, 2, 3, 4]
+let array2 = [5, 6, 7, 8]
+let combinedArray = [...array1, ...array2]
+
+console.log(combinedArray)
+
+function sum(a, b, c) {
+  return a + b + c
+}
+
+let sumArray = [10, 20, 30]
+const result = sum(...sumArray)
+console.log(result)
+
+// Ternary Operator
+
+//example:
+let checkNum = (num) => {
+  return num % 2 === 0 ? "Even" : "Odd"
+
+  // this is the same
+  //   if (num % 2 === 0) {
+  //     return "Even"
+  //   } else {
+  //     return "Odd"
+  //   }
+}
+
+console.log(checkNum(14))
+
+// Destrcturing
+let [firstNumber, secondNumber, thirdNumber] = [1, 2, 9]
+
+// kje go preskokne vtoriot element od nizata
+// let [firstNumber, , thirdNumber] = [1, 2, 9]
+console.log(firstNumber, thirdNumber)
+
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 33,
+}
+
+const { firstName, age: personAge } = person
+console.log(firstName, personAge)
