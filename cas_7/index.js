60d249fab02a5e6ce75ad82d85bd14870f44e0f6
hristijankostadinoski <hristijan@2coders.com>
2023-11-20 20:59:11 +0100

    event listeners - part 1

---
+++ cas_7/index.js
@@ -0,0 +1,42 @@
+// Event Listeners
+const myButton = document.getElementById("my-button")
+
+const clickHandler = () => {
+  alert("The button is clicked!")
+  //   myButton.removeEventListener("click", clickHandler) // removing the event listener
+}
+
+myButton.addEventListener("click", clickHandler)
+
+const title = document.querySelector("h1")
+
+title.addEventListener("", () => {
+  title.style.fontSize = "45px"
+  title.style.transition = "font-size 0.3s ease-in-out"
+})
+
+title.addEventListener("mouseleave", () => {
+  title.style.fontSize = "25px"
+  title.style.transition = "font-size 0.3s ease-in-out"
+})
+
+// preventDefault() and stopPropagation()
+const googleLink = document.getElementsByTagName("a")[0]
+googleLink.addEventListener("click", (event) => {
+  event.preventDefault()
+  console.log(event)
+  console.log("google link")
+  googleLink.style.color = "green"
+})
+
+const parent = document.querySelector(".parent")
+const child = document.querySelector(".child")
+
+parent.addEventListener("click", () => {
+  console.log("you clicked the PARENT element")
+})
+
+child.addEventListener("click", (event) => {
+  event.stopPropagation()
+  console.log("you clicked the CHILD element")
+})
